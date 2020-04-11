ca490b7fbd79e443c94a9b7787876763da049539
James Saunders <james@saunders-family.net>
2020-04-11 09:17:34 +0100

    Improvements to keyboard code.

--- midi_keyboard/midi_keyboard.ino
+++ midi_keyboard/midi_keyboard.ino
@@ -1,4 +1,9 @@
-#define NUM_BUTTONS  7
+# include <MIDI.h>
+# include "midiNote2Frequency.h"
+
+#define NUM_BUTTONS 7
+#define LED_PIN 13    // Arduino Board LED is on Pin 13
+#define BUZZER_PIN 9
 
 const uint8_t button1 = 2;
 const uint8_t button2 = 3;
@@ -7,16 +12,10 @@ const uint8_t button4 = 5;
 const uint8_t button5 = 6;
 const uint8_t button6 = 7;
 const uint8_t button7 = 8;
-
 const int intensityPot = 0;  // A0 input
 
 const uint8_t buttons[NUM_BUTTONS] = {button1, button2, button3, button4, button5, button6, button7};
-// const byte notePitches[NUM_BUTTONS] = {C3, D3, E3, F3, G3, A3, B3};
-
-uint8_t notesTime[NUM_BUTTONS];
-uint8_t pressedButtons = 0x00;
-uint8_t previousButtons = 0x00;
-uint8_t intensity;
+const uint8_t midiNotes[NUM_BUTTONS] = {57, 59, 60, 62, 64, 65, 67};
 
 void setup() {
   Serial.begin(9600);
@@ -25,22 +24,17 @@ void setup() {
     pinMode(buttons[i], INPUT_PULLUP);
 }
 
-
 void loop() {
   readButtons();
   readIntensity();
-  playNotes();
 }
-
 
 void readButtons() {
   for (int i = 0; i < NUM_BUTTONS; i++) {
+    int midiNote = midiNotes[i];
     if (digitalRead(buttons[i]) == LOW) {
-      Serial.print("Note: ");
-      Serial.print(i);
-      Serial.println();
-    } else {
-      
+      digitalWrite(LED_PIN, LOW);
+      MyHandleNoteOn(0, midiNote, 0);
     }
   }
 }
@@ -54,6 +48,15 @@ void readIntensity() {
   }
 }
 
-void playNotes() {
+void MyHandleNoteOn(byte channel, byte midiNote, byte velocity) {
+  digitalWrite(LED_PIN, HIGH);
+  Serial.print("Note: ");
+  Serial.print(midiNote);
+  Serial.println();
+  int frequency = FreqFromMidiNote(midiNote);
+  tone(BUZZER_PIN, frequency, 500);
+}
 
+void MyHandleNoteOff(byte channel, byte miniNote, byte velocity) {
+  noTone(BUZZER_PIN);
 }
