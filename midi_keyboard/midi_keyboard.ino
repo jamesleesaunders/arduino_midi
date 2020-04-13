b6d132799de7b4bfc74934684e9234f5ed7d5872
James Saunders <james@saunders-family.net>
2020-04-13 22:29:37 +0100

    Got pitch bend working.

--- midi_keyboard/midi_keyboard.ino
+++ midi_keyboard/midi_keyboard.ino
@@ -9,7 +9,7 @@
 
 #define NUM_BUTTONS 7
 #define LED_PIN 13
-#define BUZZER_PIN 9
+#define PIEZO_PIN 9
 
 const uint8_t button1 = 2;
 const uint8_t button2 = 3;
@@ -18,12 +18,12 @@ const uint8_t button4 = 5;
 const uint8_t button5 = 6;
 const uint8_t button6 = 7;
 const uint8_t button7 = 8;
-const int intensityPot = 0;  // A0 input
+const int pitchPot = 0;  // A0 input
 
 const uint8_t buttons[NUM_BUTTONS] = {button1, button2, button3, button4, button5, button6, button7};
 const uint8_t midiNotes[NUM_BUTTONS] = {57, 59, 60, 62, 64, 65, 67};
 
-uint8_t intensity;
+int midiPitch;
 
 void setup() {
   Serial.begin(9600);
@@ -34,11 +34,15 @@ void setup() {
 }
 
 void loop() {
-  readButtons();
-  readIntensity();
+  readKeys();
+  readPitch();
 }
 
-void readButtons() {
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void readKeys() {
   for (int i = 0; i < NUM_BUTTONS; i++) {
     int midiNote = midiNotes[i];
     if (digitalRead(buttons[i]) == LOW) {
@@ -48,30 +52,37 @@ void readButtons() {
   }
 }
 
-void readIntensity() {
-  int intensity = analogRead(intensityPot);
-  setIntensity(intensity);
+void readPitch() {
+  int pitch = analogRead(pitchPot);
+  setPitch(pitch);
 }
 
 void MyHandleNoteOn(byte channel, byte midiNote, byte velocity) {
-  digitalWrite(LED_PIN, HIGH);
-  Serial.print("Note: ");
-  Serial.print(midiNote);
-  Serial.println();
+  // Lookup frequency for MIDI Note
   int frequency = FreqFromMidiNote(midiNote);
-  tone(BUZZER_PIN, frequency, 500);
+
+  // Difference between notes is ~6%
+  float pitchShift = mapfloat(midiPitch, 0, 127, 0.06, 1.06);
+  frequency = frequency * pitchShift;
+    
+  Serial.print("Note: ");
+  Serial.println(midiNote);
+  Serial.print("Pitch Shift: ");
+  Serial.println(pitchShift);
+  Serial.print("Frequency: ");
+  Serial.println(frequency);
+
+  digitalWrite(LED_PIN, HIGH);
+  tone(PIEZO_PIN, frequency, 100);
 }
 
-void MyHandleNoteOff(byte channel, byte miniNote, byte velocity) {
-  noTone(BUZZER_PIN);
+void MyHandleNoteOff(byte channel, byte midiNote, byte velocity) {
+  noTone(PIEZO_PIN);
 }
 
-void setIntensity(byte val) {
+void setPitch(byte val) {
   if (val < 100) {
     val = 100;
   }
-  intensity = (uint8_t) (map(val, 100, 255, 0, 127));
-  Serial.print("Intensity: ");
-  Serial.print(intensity);
-  Serial.println();
+  midiPitch = (uint8_t) (map(val, 100, 255, 0, 127));
 }
