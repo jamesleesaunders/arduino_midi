adf7ee3cdde093133fa6cf28411bc1e00bdc7dc1
James Saunders <james@saunders-family.net>
2021-11-20 16:54:50 +0000

    Updated XMas Tree LED  sketches.

--- midi_xmas_led/midi_xmas_led.ino
+++ midi_xmas_led/midi_xmas_led.ino
@@ -1,12 +1,12 @@
 /*************************************************
- * MIDI In LED Example
- *
- * Author: James Saunders
+   MIDI Christmas Tree LED
+
+   Author: James Saunders
  *************************************************/
 
 #include <MIDI.h>
 
-// Created and binds the MIDI interface to the default hardware serial port.
+// Created and binds the MIDI interface to the default hardware serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 #define LED_C 6
@@ -20,7 +20,7 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 typedef struct {
   int midiNote;
   int led;
-} MidiNotes;
+} MidiLed;
 
 #define MIDI_G9  127
 #define MIDI_F9  125
@@ -88,80 +88,80 @@ typedef struct {
 
 #define NUM_NOTES 63
 
-MidiNotes noteLookup[NUM_NOTES] = {
-    {MIDI_G9,  LED_G},
-    {MIDI_F9,  LED_F},
-    {MIDI_E9,  LED_E},
-    {MIDI_D9,  LED_D},
-    {MIDI_C9,  LED_C},
-    {MIDI_B8,  LED_B},
-    {MIDI_A8,  LED_A},
-    {MIDI_G8,  LED_G},
-    {MIDI_F8,  LED_F},
-    {MIDI_E8,  LED_E},
-    {MIDI_D8,  LED_D},
-    {MIDI_C8,  LED_C},
-    {MIDI_B7,  LED_B},
-    {MIDI_A7,  LED_A},
-    {MIDI_G7,  LED_G},
-    {MIDI_F7,  LED_F},
-    {MIDI_E7,  LED_E},
-    {MIDI_D7,  LED_D},
-    {MIDI_C7,  LED_C},
-    {MIDI_B6,  LED_B},
-    {MIDI_A6,  LED_A},
-    {MIDI_G6,  LED_G},
-    {MIDI_F6,  LED_F},
-    {MIDI_E6,  LED_E},
-    {MIDI_D6,  LED_D},
-    {MIDI_C6,  LED_C},
-    {MIDI_B5,  LED_B},
-    {MIDI_A5,  LED_A},
-    {MIDI_G5,  LED_G},
-    {MIDI_F5,  LED_F},
-    {MIDI_E5,  LED_E},
-    {MIDI_D5,  LED_D},
-    {MIDI_C5,  LED_C},
-    {MIDI_B4,  LED_B},
-    {MIDI_A4,  LED_A},
-    {MIDI_G4,  LED_G},
-    {MIDI_F4,  LED_F},
-    {MIDI_E4,  LED_E},
-    {MIDI_D4,  LED_D},
-    {MIDI_C4,  LED_C},
-    {MIDI_B3,  LED_B},
-    {MIDI_A3,  LED_A},
-    {MIDI_G3,  LED_G},
-    {MIDI_F3,  LED_F},
-    {MIDI_E3,  LED_E},
-    {MIDI_D3,  LED_D},
-    {MIDI_C3,  LED_C},
-    {MIDI_B2,  LED_B},
-    {MIDI_A2,  LED_A},
-    {MIDI_G2,  LED_G},
-    {MIDI_F2,  LED_F},
-    {MIDI_E2,  LED_E},
-    {MIDI_D2,  LED_D},
-    {MIDI_C2,  LED_C},
-    {MIDI_B1,  LED_B},
-    {MIDI_A1,  LED_A},
-    {MIDI_G1,  LED_G},
-    {MIDI_F1,  LED_F},
-    {MIDI_E1,  LED_E},
-    {MIDI_D1,  LED_D},
-    {MIDI_C1,  LED_C},
-    {MIDI_B0,  LED_B},
-    {MIDI_A0,  LED_A}
+MidiLed noteLookup[NUM_NOTES] = {
+  {MIDI_G9,  LED_G},
+  {MIDI_F9,  LED_F},
+  {MIDI_E9,  LED_E},
+  {MIDI_D9,  LED_D},
+  {MIDI_C9,  LED_C},
+  {MIDI_B8,  LED_B},
+  {MIDI_A8,  LED_A},
+  {MIDI_G8,  LED_G},
+  {MIDI_F8,  LED_F},
+  {MIDI_E8,  LED_E},
+  {MIDI_D8,  LED_D},
+  {MIDI_C8,  LED_C},
+  {MIDI_B7,  LED_B},
+  {MIDI_A7,  LED_A},
+  {MIDI_G7,  LED_G},
+  {MIDI_F7,  LED_F},
+  {MIDI_E7,  LED_E},
+  {MIDI_D7,  LED_D},
+  {MIDI_C7,  LED_C},
+  {MIDI_B6,  LED_B},
+  {MIDI_A6,  LED_A},
+  {MIDI_G6,  LED_G},
+  {MIDI_F6,  LED_F},
+  {MIDI_E6,  LED_E},
+  {MIDI_D6,  LED_D},
+  {MIDI_C6,  LED_C},
+  {MIDI_B5,  LED_B},
+  {MIDI_A5,  LED_A},
+  {MIDI_G5,  LED_G},
+  {MIDI_F5,  LED_F},
+  {MIDI_E5,  LED_E},
+  {MIDI_D5,  LED_D},
+  {MIDI_C5,  LED_C},
+  {MIDI_B4,  LED_B},
+  {MIDI_A4,  LED_A},
+  {MIDI_G4,  LED_G},
+  {MIDI_F4,  LED_F},
+  {MIDI_E4,  LED_E},
+  {MIDI_D4,  LED_D},
+  {MIDI_C4,  LED_C},
+  {MIDI_B3,  LED_B},
+  {MIDI_A3,  LED_A},
+  {MIDI_G3,  LED_G},
+  {MIDI_F3,  LED_F},
+  {MIDI_E3,  LED_E},
+  {MIDI_D3,  LED_D},
+  {MIDI_C3,  LED_C},
+  {MIDI_B2,  LED_B},
+  {MIDI_A2,  LED_A},
+  {MIDI_G2,  LED_G},
+  {MIDI_F2,  LED_F},
+  {MIDI_E2,  LED_E},
+  {MIDI_D2,  LED_D},
+  {MIDI_C2,  LED_C},
+  {MIDI_B1,  LED_B},
+  {MIDI_A1,  LED_A},
+  {MIDI_G1,  LED_G},
+  {MIDI_F1,  LED_F},
+  {MIDI_E1,  LED_E},
+  {MIDI_D1,  LED_D},
+  {MIDI_C1,  LED_C},
+  {MIDI_B0,  LED_B},
+  {MIDI_A0,  LED_A}
 };
 
-int ledFromMidiNote(int midiNote) {
+MidiLed ledFromMidiNote(int midiNote) {
   for (int i = 0; i < NUM_NOTES; i++) {
     if (noteLookup[i].midiNote == midiNote) {
-      return noteLookup[i].led;
+      return noteLookup[i];
     }
   }
 
-  return 0;
+  return;
 }
 
 void setup() {
@@ -174,10 +174,10 @@ void setup() {
   pinMode(LED_F, OUTPUT);
   pinMode(LED_G, OUTPUT);
 
+  // Initialize the MIDI Library
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
   MIDI.setHandleNoteOn(handleNoteOn);
-
   MIDI.setHandleNoteOff(handleNoteOff);
 }
 
@@ -186,11 +186,11 @@ void loop() {
 }
 
 void handleNoteOn(byte channel, byte midiNote, byte velocity) {
-  int led = ledFromMidiNote(midiNote);
+  int led = ledFromMidiNote(midiNote).led;
   digitalWrite(led, HIGH);
 }
 
 void handleNoteOff(byte channel, byte midiNote, byte velocity) {
-  int led = ledFromMidiNote(midiNote);
+  int led = ledFromMidiNote(midiNote).led;
   digitalWrite(led, LOW);
 }
